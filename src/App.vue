7087ea098d93aa339c55cae4218d604bbd923a40
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2021-09-27 01:49:49 +0200

    Add basic assets

--- src/App.vue
+++ src/App.vue
@@ -7,3 +7,18 @@
         name: "App"
     }
 </script>
+
+<style lang="scss" scoped>
+    @use "assets/scss/utilities/colors";
+    @use "assets/scss/utilities/fonts";
+
+    h1 {
+        padding: 10px;
+
+        background-color: colors.$Primary-Light-red;
+
+        text-align: center;
+        color: colors.$White;
+        font-family: fonts.$Ubuntu;
+    }
+</style>
