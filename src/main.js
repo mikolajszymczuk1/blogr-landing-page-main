3f870bb0db10b7869ad123e24a02f515867a2503
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2021-10-03 22:23:14 +0200

    Add Vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "./assets/scss/main.scss";
+import Vuex from "vuex";
 
 Vue.config.productionTip = false;
+
+Vue.use(Vuex);
 
 new Vue({
   render: (h) => h(App),
