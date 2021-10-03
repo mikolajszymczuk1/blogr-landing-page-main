dddd6b3136d88610cc1f66dc0108de14de06698d
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2021-10-03 22:43:02 +0200

    Add no-resize option (prop)

--- src/components/ui/LogoImage.vue
+++ src/components/ui/LogoImage.vue
@@ -1,12 +1,17 @@
 <template>
     <a class="logo-link" href="#">
-        <img class="logo-link__image" src="@/assets/svg/logo.svg" alt="Logo" title="Logo">
+        <img class="logo-link__image" src="@/assets/svg/logo.svg" alt="Logo" title="Logo"
+            :class="noResize ? 'logo-link__image--no-resize' : ''"
+        >
     </a>
 </template>
 
 <script>
 export default {
     name: "LogoImage",
+    props: {
+        noResize: Boolean
+    }
 }
 </script>
 
@@ -19,6 +24,10 @@ export default {
         height: auto;
 
         @media screen and (min-width: 1024px) {
+            width: 95px;
+        }
+
+        &--no-resize {
             width: 95px;
         }
     }
