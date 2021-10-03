8e6967e0656c9c08528a9f3159feecba8c681d92
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2021-10-03 22:42:28 +0200

    Fix imports

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "./assets/scss/main.scss";
-import Vuex from "vuex";
 
 Vue.config.productionTip = false;
-
-Vue.use(Vuex);
 
 new Vue({
   render: (h) => h(App),
